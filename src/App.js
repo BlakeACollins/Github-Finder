b0f2c0488cd16274917799daca46352a067b44ff
Blake Collins <blakeac369@gmail.com>
2021-01-12 14:37:04 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import Navbar from './components/layouts/Navbar';
 import './App.css';
+import Users from './components/layouts/users/User'
 
 class App extends React.Component{
 
@@ -10,6 +11,10 @@ class App extends React.Component{
     return (
     <div className="App">
       <Navbar/>
+      <div className='container'>
+      <Users/>
+      </div>
+      
     </div>
   );
 }
