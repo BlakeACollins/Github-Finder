b376574a5d8a87a16f79da86df6a7e01ec31ece7
Blake Collins <blakeac369@gmail.com>
2021-01-12 20:48:39 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import React, { Component }from 'react'
-import Navbar from './components/layouts/Navbar';
-import './App.css';
-import Search from './components/layouts/users/Search';
+import PropTypes from 'prop-types';
 import axios from 'axios';
+import './App.css';
+import Navbar from './components/layouts/Navbar';
+import Search from './components/layouts/users/Search';
 import Users from './components/layouts/users/User';
 
 class App extends Component{
@@ -11,6 +12,14 @@ class App extends Component{
     users: [],
     loading: false
   };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  };
+
+
+
+// This code block will auto load 30 profiles from GitHub
 
 // async componentDidMount(){
 // this.setState({ loading: true});
@@ -22,6 +31,7 @@ class App extends Component{
 
   //Search github
   searchUsers = async text => {
+    this.setState({ loading: true });
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({users: res.data.items, loading: false});
