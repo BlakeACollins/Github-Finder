d8afa5683697ff5a281bf1d2326fca940cb503e8
Blake Collins <blakeac369@gmail.com>
2021-01-12 20:43:15 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,14 +10,21 @@ class App extends Component{
   state = {
     users: [],
     loading: false
-  }
-async componentDidMount(){
+  };
 
-this.setState({ loading: true});
+// async componentDidMount(){
+// this.setState({ loading: true});
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+//     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({users: res.data, loading: false});
+//     this.setState({users: res.data, loading: false});
+//   };
+
+  //Search github
+  searchUsers = async text => {
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({users: res.data.items, loading: false});
   }
 
 
@@ -28,7 +35,7 @@ this.setState({ loading: true});
     <div className="App">
       <Navbar/>
       <div className='container'>
-        <Search />
+        <Search searchUsers={this.searchUsers}/>
       <Users loading={this.state.loading} users={this.state.users}/>
       </div>
       
