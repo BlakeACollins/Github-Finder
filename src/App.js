8ac38abccc08232bf1e4b35b1bbf4fde7f51ab1e
Blake Collins <blakeac369@gmail.com>
2021-01-13 15:22:28 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './App.css';
 import Navbar from './components/layouts/Navbar';
 import Search from './components/layouts/users/Search';
 import Users from './components/layouts/users/User';
+import Alert from './components/layouts/Alert';
 
 class App extends Component{
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   static propTypes = {
@@ -37,16 +39,35 @@ class App extends Component{
     this.setState({users: res.data.items, loading: false});
   }
 
+  //Clear Users from search and current state.
+  clearUsers = () => this.setState({ users: [], loading: false });
 
+  //Alert if search bar is not filled out
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg,type }});
+    setTimeout(() => this.setState({ alert: null}), 4000)
+  };
+  
 
   render() {
+
+    const {users, loading} = this.state;
       
     return (
     <div className="App">
       <Navbar/>
       <div className='container'>
-        <Search searchUsers={this.searchUsers}/>
-      <Users loading={this.state.loading} users={this.state.users}/>
+        <Alert alert={this.state.alert} />
+        <Search 
+        searchUsers={this.searchUsers} 
+        clearUsers={this.clearUsers} 
+        showClear ={users.lenght > 0 ? true: false} 
+        setAlert={this.setAlert}
+        />
+
+        <Users 
+        loading={loading} 
+        users={users}/>
       </div>
       
     </div>
