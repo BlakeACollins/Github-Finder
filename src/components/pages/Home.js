c03b54179993acb169562ab64f54ca76a5b1c6d4
Blake Collins <blakeac369@gmail.com>
2021-01-15 13:13:43 -0800

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,10 +3,12 @@ import Search from '../users/Search';
 import Users from '../users/Users'
 
 const Home = () => {
+    return(
     <Fragment>
         <Search />
         <Users />
     </Fragment>
+   );
 };
 
 export default Home;
