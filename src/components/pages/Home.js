df8b3fdf72ec08c1440e4504304f1a52e7eb41a2
Blake Collins <blakeac369@gmail.com>
2021-01-16 20:35:45 -0800

    Add files via upload

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,12 @@ import Search from '../users/Search';
 import Users from '../users/Users'
 
 const Home = () => {
-    return(
+    return (
     <Fragment>
         <Search />
         <Users />
     </Fragment>
-   );
+    );
 };
 
 export default Home;
