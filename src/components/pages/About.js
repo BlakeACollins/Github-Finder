963ca474502429a15fff788b01dc2bfcbc07e9af
Blake Collins <blakeac369@gmail.com>
2021-04-14 20:06:41 -0700

    Add files via upload

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,7 @@ const About = () => {
     return (
         <Fragment>
             <h1>About This App</h1>
-            <p>This app is made with React to search GitHub for users and their profiles!</p>
-            <p>Version 1</p>
+            <p>This is a React app that pulls GitHub profiles and their most recent repositories. Search for any developer you want!</p>
         </Fragment>
     )
 }
